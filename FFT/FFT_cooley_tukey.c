0df236b20d933359cf229ba5610980e8bd8d08b2
chakri1804 <chakri1804@gmail.com>
2018-10-07 15:51:57 +0530

    Huehuehue

--- FFT/FFT_cooley_tukey.c
+++ FFT/FFT_cooley_tukey.c
@@ -10,18 +10,17 @@ clx* fft(clx buf[],int n){
   return buf;
   clx wn = cexp(-I*2*pi/n);
   clx w  = 1.0000000;
-  clx Odd[n/2];
-  clx Even[n/2];
-  for (int i = 0; i<n/2; i++){
-    Even[i] = buf[2*i];
-    Odd[i]  = buf[2*i+1];
-  }
   clx *Ye=(clx*)malloc((n/2)*sizeof(clx));
   clx *Yo=(clx*)malloc((n/2)*sizeof(clx));
+  int i=0;
+  for (i = 0; i<n/2; i++){
+    Ye[i] = buf[2*i];
+    Yo[i]  = buf[2*i+1];
+  }
   clx *y=(clx*)malloc((n)*sizeof(clx));
-  Yo = fft(Odd,n/2);
-  Ye = fft(Even,n/2);
-  for(int i=0; i<n/2; i++){
+  Yo = fft(Yo,n/2);
+  Ye = fft(Ye,n/2);
+  for(i=0; i<n/2; i++){
     y[i] = Ye[i] + (w*Yo[i]);
     y[i+(n/2)] = Ye[i] - (w*Yo[i]);
     w = w*wn;
@@ -30,7 +29,8 @@ clx* fft(clx buf[],int n){
 }
 
 void show(clx buf[], int n) {
-	for (int i = 0; i < n; i++)
+  int i;
+	for (i = 0; i < n; i++)
 		if (!cimag(buf[i]))
 			printf("%g ", creal(buf[i]));
 		else
